5f93c346219c11ea25fad2dad22b4a2d81783a51
Garik Aghayan <11.aghayan.99@gmail.com>
2024-04-13 17:00:19 +0400

    item href added

--- src/pages/landing/components/content/components/items/components/item/Item.tsx
+++ src/pages/landing/components/content/components/items/components/item/Item.tsx
@@ -1,18 +1,31 @@
-import { ItemType } from '../../../../../../../../types';
+import { useLocation } from 'react-router-dom';
+
 import './Item.css';
+
+import { ItemType } from '../../../../../../../../types';
 
 type Props = {
   item: ItemType
 }
 
 export default function Item({ item }: Props) {
+  const { pathname } = useLocation();
   
   return (
     <li className='item'>
-      <img src={item.photos[0].src} alt="" height={100} width={100} />
-      <p>{item.name}</p>
-      <p>{item.price}</p>
-      <p>{item.size.val}</p>
+      <a href={`${pathname}/${item.id}`}>
+        <div className='item-photo'>
+          <img 
+            src={item.photos[0].src} 
+            alt="item photo" 
+            height={100} 
+            width={100} 
+          />
+        </div>
+        <p>{item.name}</p>
+        <p>{item.price}</p>
+        <p>{item.size.val}</p>
+      </a>
     </li>
   );
 }
