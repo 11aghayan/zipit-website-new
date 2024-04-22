bec36a17282985433a9f582d46123aeab7d33376
Garik Aghayan <11.aghayan.99@gmail.com>
2024-04-22 04:19:05 +0400

    item name length handled

--- src/pages/landing/components/content/components/items/components/item/components/itemName/ItemName.tsx
+++ src/pages/landing/components/content/components/items/components/item/components/itemName/ItemName.tsx
@@ -6,10 +6,14 @@ type Props = {
 
 export default function ItemName({ name }: Props) {
 
-  const isLong = name.length > 10;
+  const isLongDesktop = name.length > 30;
+  const isLongTablet = name.length > 20;
+  const isLongMobile = name.length > 10;
+
+  const long = isLongDesktop ? 'long-desktop' : isLongTablet ? 'long-tablet' : isLongMobile ? 'long-mobile' : '';
   
   return (
-    <h3 className={`item-name ${isLong ? 'long' : ''}`}>
+    <h3 className={`item-name ${long}`}>
       {name}
     </h3>
   );
