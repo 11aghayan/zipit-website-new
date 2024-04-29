b1264aba230dc46f87bceec2a0d74b30f6303373
Garik Aghayan <11.aghayan.99@gmail.com>
2024-04-30 00:35:44 +0400

    Maps: lat lng set; readme changed;

--- src/pages/contacts/components/map/Map.tsx
+++ src/pages/contacts/components/map/Map.tsx
@@ -3,8 +3,8 @@ import './Map.css';
 import {APIProvider, Map as GoogleMap, AdvancedMarker } from '@vis.gl/react-google-maps';
 
 export default function Map() {
-  const position = { lat: 40.169771, lng: 44.522061 };
-
+  const position = { lat: 40.169634, lng: 44.522070 };
+  
   return (
     <section className='map'>
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
