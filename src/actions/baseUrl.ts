f0617c657cf38b467f91dcefc4ea793687479c80
Garik Aghayan <11.aghayan.99@gmail.com>
2024-05-17 00:09:03 +0400

    baseUrl switched to production

--- src/actions/baseUrl.ts
+++ src/actions/baseUrl.ts
@@ -1,7 +1,7 @@
 //Test URL
-const baseUrl = 'http://localhost:3200';
+// const baseUrl = 'http://localhost:3200';
 
 // Production URL
-// const baseUrl = 'https://zipit-server.onrender.com'; 
+const baseUrl = 'https://zipit-server.onrender.com'; 
 
 export default baseUrl;
