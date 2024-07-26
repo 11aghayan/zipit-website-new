e5061f65bd0e6d4a7dd3035c66f245b9e482f8d4
Garik Aghayan <11.aghayan.99@gmail.com>
2024-07-26 17:18:23 +0400

    announcement added

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -3,6 +3,7 @@ import './CartPage.css';
 import useCart from '../../hooks/useCart';
 import Content from './components/content/Content';
 import Header from './components/header/Header';
+import Announcement from './components/announcement/Announcement';
 
 export default function CartPage() {
   const cartEmpty = useCart()[0].length < 1;
@@ -17,6 +18,7 @@ export default function CartPage() {
         :
         <Content />
       }
+      <Announcement />
     </main>
   );
 }
---
+++ src/pages/cart/components/announcement/Announcement.tsx
@@ -0,0 +1,38 @@
+import './Announcement.css';
+
+import { useState } from 'react';
+
+import { Icon } from '@iconify/react/dist/iconify.js';
+import useLang from '../../../../hooks/useLang';
+import { LangType } from '../../../../types';
+
+export default function Announcement() {
+  const lang = useLang() as LangType;
+  
+  const headerText = {
+    am: 'Ուշադրություն',
+    ru: 'Внимание'
+  };
+  const bodyText = {
+    am: '30000 դրամ և ավել պատվերների դեպքում առաքումն անվճար է',
+    ru: 'При заказе на сумму 30000 драм и более доставка бесплатная'
+  };
+  
+  const [isOpen, setIsOpen] = useState(true);
+
+  const close = () => {
+    setIsOpen(false);
+  };
+  
+  return (
+    <div className={`announcement ${isOpen ? '' : 'closed'}`}>
+      <div className='top'>
+        <p className='header'>{headerText[lang]}</p>
+        <button className='close-btn' onClick={close}>
+          <Icon icon='lucide:x' />
+        </button>
+      </div>
+      <p className='body'>{bodyText[lang]}</p>
+    </div>
+  );
+}
