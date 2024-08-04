eff6650bdaca1409a07a2683b2abe3bd9292b712
11aghayan <11.aghayan.99@gmail.com>
2024-08-04 15:37:03 +0400

    prod url corrected

--- src/actions/baseUrl.ts
+++ src/actions/baseUrl.ts
@@ -2,6 +2,6 @@
 // const baseUrl = 'http://localhost:3200/api/v1';
 
 // Production URL
-const baseUrl = 'https://zipit-server.onrender.com';
+const baseUrl = 'https://zipit-server.onrender.com/api/v1';
 
 export default baseUrl;
