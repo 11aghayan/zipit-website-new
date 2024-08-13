510b2bef04a7fc02e8bc5275864b5d9843c2aaa1
11aghayan <11.aghayan.99@gmail.com>
2024-08-14 01:58:16 +0400

    suggestions added to item page; getSimilarItems axios modified

--- src/actions/getSimilarItems.ts
+++ src/actions/getSimilarItems.ts
@@ -7,12 +7,15 @@ import CustomError from "./customError";
 export default async function getSimilarItems(category: string, name: string, lang: LangType) {
   const url = `${baseUrl}/items/${lang}/similar`;
   try {
-    const res = await axios.get(url, {
-      data: {
-        category,
+    const res = await axios.post(url, {
+        categoryId: category,
         name
+      }, {
+        headers: {
+          "Content-Type": 'application/json'
+        }
       }
-    });
+    );
     const { data } = res;
     
     if (res.status !== 200) {
--- src/pages/item/components/item/Item.tsx
+++ src/pages/item/components/item/Item.tsx
@@ -3,6 +3,7 @@ import './Item.css';
 import { ItemType } from '../../../../types';
 import LeftSide from './components/leftSide/LeftSide';
 import RightSide from './components/rightSide/RightSide';
+import Suggestions from '../../../../components/suggestions/Suggestions';
 
 type Props = {
   item: ItemType;
@@ -12,8 +13,11 @@ export default function Item({ item }: Props) {
   
   return (
     <article className='item-page--item'>
-      <LeftSide item={item} />
-      <RightSide item={item} />
+      <section className='main'>
+        <LeftSide item={item} />
+        <RightSide item={item} />
+      </section>
+      <Suggestions type='similar' />
     </article>
   );
 }
