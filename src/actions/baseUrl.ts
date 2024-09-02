d8c57544ae5cb576dfdea0d50d3b41d0cac23a4a
11aghayan <11.aghayan.99@gmail.com>
2024-09-02 15:09:40 +0400

    api base url updated

--- src/actions/baseUrl.ts
+++ src/actions/baseUrl.ts
@@ -2,6 +2,6 @@
 // const baseUrl = 'http://localhost:3200/api/v1';
 
 // Production URL
-const baseUrl = 'https://api.garikaghayan.top/api/v1';
+const baseUrl = 'https://www.api.garikaghayan.top/api/v1';
 
 export default baseUrl;
