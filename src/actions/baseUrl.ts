69a8aad0778b806aa69ddee448118577819bdb43
11aghayan <11.aghayan.99@gmail.com>
2024-11-28 13:36:49 +0400

    server base url changed

--- src/actions/baseUrl.ts
+++ src/actions/baseUrl.ts
@@ -2,6 +2,6 @@
 // const baseUrl = 'http://localhost:3200/api/v1';
 
 // Production URL
-const baseUrl = 'https://zipit-public.onrender.com/api/v1';
+const baseUrl = 'https://zipit-server.onrender.com/api/v1';
 
 export default baseUrl;
